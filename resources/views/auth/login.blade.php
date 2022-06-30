e1e6d457217d08ab6bf16b92ac1e9b1efd13b0e3
André Gregoletto <argregoletto@gmail.com>
2022-06-30 15:16:49 -0300

    login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,46 @@
 <x-guest-layout>
-    <div class="login-box">
+    <div class="login-box container">
         <div class="login-logo">
-            <a href="#"><b>Admin</b>LTE</a>
+            <a href="#"><p class="text-center mb-2 mt-3"><b>CARDÁPIO</b>online</p></a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg text-center mb-3">Sign in to start your session</p>
 
             <form action="{{route('login')}}" method="post">
                 @csrf
                 <div class="input-group mb-3">
-                    <input type="email" class="form-control" name="email" placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
+                    <input type="email" class="form-control @error('email') is-Invalid @enderror" name="email" placeholder="E-mail">
                 </div>
+                @error('email')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="Password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
+                    <input type="password" class="form-control @error('password') is-Invalid @enderror" name="password" placeholder="Senha">
                 </div>
+                @error('password')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
                 <div class="row">
                     <div class="col-8">
                         <div class="icheck-primary">
                         <input type="checkbox" name="rememberme" id="remember">
                         <label for="remember">
-                            Remember Me
+                            Lembrar-me
                         </label>
                     </div>
                 </div>
                 <!-- /.col -->
                 <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    <button type="submit" class="btn btn-outline-primary btn-block bg-primary text-white">Entrar</button>
                 </div>
                 <!-- /.col -->
                 </div>
             </form>
 
             <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
+                <p class="mb-3">- Ou -</p>
                 <a href="#" class="btn btn-block btn-primary">
                 <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
                 </a>
@@ -55,10 +51,7 @@
             <!-- /.social-auth-links -->
 
             <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
+                <a href="{{route('password.request')}} text-left">Esqeuceu a senha?</a>
             </p>
             </div>
             <!-- /.login-card-body -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,15 +4,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+        <link rel="stylesheet" href="{{ mix('css/fontawesome.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/icheck-bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+    <body class="bg-light container hold-transition login-page">
+        <div class="font-sans text-gray-900 antialiased row mt-5">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">{{ $slot }}</div>
+            <div class="col-md-3"></div>
         </div>
 
         <!-- Scripts -->
